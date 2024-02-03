3e49c1209e34cb660b866137de2f78952cf91547
Vytautas A <vytautasasm@gmail.com>
2024-02-03 13:36:07 -0600

    initial commit

---
+++ src/main/java/com/theateam/waaprojectspringbackend/WaaProjectSpringBackendApplication.java
@@ -0,0 +1,13 @@
+package com.theateam.waaprojectspringbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaaProjectSpringBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaaProjectSpringBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/theateam/waaprojectspringbackend/WaaProjectSpringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theateam.waaprojectspringbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaaProjectSpringBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
