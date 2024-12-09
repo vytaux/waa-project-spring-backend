ac97e7a7c1c310b86540af9083e62a1dbad6bf55
Vytautas A. <vytautasasm@gmail.com>
2024-12-08 21:12:29 -0600

    TEst cors.

---
+++ src/main/java/com/theateam/waaprojectspringbackend/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.theateam.waaprojectspringbackend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "https://master.d3lwkpamsygywc.amplifyapp.com"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
